fda1b7f0c748b5b96e8ad46af00c3e764d1f8e8a
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2020-03-20 21:41:36 +0100

    feat: add model for group

---
+++ core/src/main/scala/scim/model/Group.scala
@@ -0,0 +1,31 @@
+package scim.model
+
+import io.circe.Json
+import io.circe.generic.auto._
+import scim.model.Group.Root
+
+case class Group(json: Json) extends ExtensibleModel[Root] {
+  def schema: Schema = Schema.Group
+  lazy val root: Root = json.as[Root].toOption.getOrElse(Root.fallback)
+
+  def ++(other: Json): Group = Group(json.deepMerge(other))
+}
+
+object Group {
+  case class Root(
+    id: Option[String],
+    displayName: String,
+    members: Option[Seq[Member]] = None,
+  )
+  object Root {
+    def fallback: Root = Root(id=None, displayName = "")
+  }
+  case class Member(
+    value: String,
+    `$ref`: Option[String] = None,
+    display: Option[String] = None,
+  ) {
+    def id: String = value
+  }
+}
+
