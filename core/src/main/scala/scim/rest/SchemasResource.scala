e88b4d7d53e7063182636e43ed6e2419dacbe774
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2020-06-17 14:49:18 +0200

    fix: schema resource not as result

--- core/src/main/scala/scim/rest/SchemasResource.scala
+++ core/src/main/scala/scim/rest/SchemasResource.scala
@@ -1,24 +1,31 @@
 package scim.rest
 
+import java.net.URI
 import cats.Applicative
 import io.circe.Json
-import scim.model.{Meta, SchemaDefinition}
+import scim.model.{Filter, Meta, ResourceType, SchemaDefinition}
+import scim.rest.Helpers.Id
 import scim.rest.Resource.{Path, QueryParams}
+import scim.spi.{Paging, SearchResult, Sorting}
+import scim.spi.SpiError.DoesNotExist
 
 class SchemasResource[F[_]](urlConfig: UrlConfig, schemas: Iterable[SchemaDefinition])(implicit applicative: Applicative[F]) extends Resource[F] {
   private def pure[A]: A => F[A] = applicative.pure
 
   def get(subPath: Path, queryParams: QueryParams): F[Response] = {
-    val schemaName = Some(subPath.mkString("/")).filter(_.nonEmpty)
-    schemaName match {
-      case Some(name) =>
-        pure(schemas.find(_.schema.asString == name)
-          .map(schema => Response.ok(schema, urlConfig.base))
-          .getOrElse(Response.notFound(name)))
-      case None =>
-        val jsons = schemas.map(_.asJson(urlConfig.base)).toSeq
-        pure(Response.okJson(Json.arr(jsons: _*)))
-    }
+    Helpers.Get.retrieve(subPath, urlConfig.base)(doGet)
+      .orElse(Helpers.Get.search(subPath, queryParams, urlConfig.base)(doSearch))
+      .getOrElse(pure(Response.notImplemented))
+  }
+
+  private def doGet(id: Id): F[Either[DoesNotExist, SchemaDefinition]] = pure {
+    schemas.find(_.schema.asString.equalsIgnoreCase(id))
+      .toRight(DoesNotExist(id))
+  }
+
+  private def doSearch(filter: Filter, pageMode: Paging, sorting: Option[Sorting]): F[SearchResult[SchemaDefinition]] = pure {
+    val filtered = schemas.filter(rt => filter.evaluate(rt.asJson(URI.create("urn:none")))).toSeq
+    pageMode.applyTo(sorting.map(_.applyTo(filtered)).getOrElse(filtered))
   }
 
   def post(subPath: Path, queryParams: QueryParams, body: Json): F[Response] = pure(Response.notImplemented)
--- core/src/main/scala/scim/rest/UrlConfig.scala
+++ core/src/main/scala/scim/rest/UrlConfig.scala
@@ -4,10 +4,7 @@ import java.net.URI
 
 class UrlConfig protected(baseUrl: String) {
   val base: URI = URI.create(baseUrl)
-  def user(id: Option[String]): URI = URI.create(s"$baseUrl/Users/${id.map("/" + _).getOrElse("")}")
   def group(id: Option[String]): URI = URI.create(s"$baseUrl/Groups/${id.map("/" + _).getOrElse("")}")
-  def schema(name: Option[String]): URI = URI.create(s"$baseUrl/Schema/${name.map("/" + _).getOrElse("")}")
-  def serviceProviderConfig: URI = URI.create(s"$baseUrl/ServiceProviderConfig")
 }
 
 object UrlConfig {
